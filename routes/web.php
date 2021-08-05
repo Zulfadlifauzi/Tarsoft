559888ec214944bc4d8bd6e5fe840416415c7fde
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-08-05 21:12:04 +0800

    one to one relationship.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource("projects", ProjectController::class);
+Route::get('/user', function(){
+$users =\App\Models\User::all();
+// $addresses = \App\Models\Address::all();
+return view('users.index',compact('users'));
+
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
